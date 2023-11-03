1588e68350da4bcd4b2e47332d969693ca9da2da
Lai Quang Phat <77201894+quangphatt@users.noreply.github.com>
2023-11-03 15:54:50 +0700

    connect database

---
+++ Migrations/20231103085033_ExpenseMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpenseMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseReport",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ItemName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(10,2)", nullable: false),
+                    ExpenseDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseReport", x => x.ItemId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseReport");
+        }
+    }
+}
---
+++ Models/ExpenseDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options) : base(options)
+        { }
+
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
